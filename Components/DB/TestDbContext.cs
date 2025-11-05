5e27ac91a4c83b682a296e61a5e783529e37aa33
connerwallace <theconnerwallace@gmail.com>
2025-11-04 21:50:23 -0500

    makes checking work correctly

--- Components/DB/TestDbContext.cs
+++ Components/DB/TestDbContext.cs
@@ -10,6 +10,25 @@ namespace BlazorApp1.Components.DB
         public TestDbContext() {
             connection = new SqliteConnection("Data Source=stateinfo.db");
         }
+        
+        public List<string> query(string whereClause)
+        {
+            var returnObject = new List<string>();
+
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT name from states where " + whereClause;
+            //command.Parameters.AddWithValue("$text1", startingChars);
+            //TODO use parameters maybe
+            connection.Open();
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                returnObject.Add(reader.GetString(0).ToLowerInvariant());
+            }
+
+
+            return returnObject;
+        }
 
         public List<string> nameStartsWith(string startingChars)
         {
