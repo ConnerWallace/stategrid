384fb4da07410c8e6976f25b215eed56d56e0ba7
connerwallace <theconnerwallace@gmail.com>
2025-11-08 18:46:10 -0500

    adds gameCreator.py

---
+++ gameCreator.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+
+
+
+connection = sqlite3.connect("stateinfo.db")
+
+cursor = connection.cursor()
+
+#this table needs to exist first 
+#possibleCategories = cursor.execute("SELECT name, clause FROM categories").fetchall()
+
+#randomly select possibleCategories
+
+listOfStates = cursor.execute("SELECT name FROM STATES" .fetchall() 
+#needed cuz fetchall is returning a list of tuples
+states = list(map( lambda x: x[0], listOfStates))
+
+print(states)
+
