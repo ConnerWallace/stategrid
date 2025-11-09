571b765f6ec3cc47112fde81630a7ef0c3dfd637
connerwallace <theconnerwallace@gmail.com>
2025-11-09 18:13:17 -0500

    working generator

--- gameCreator.py
+++ gameCreator.py
@@ -1,6 +1,47 @@
 import sqlite3
+import random
+
+def allUnique(x):
+     seen = set()
+     return not any(i in seen or seen.add(i) for i in x)
+
+def CheckIfSolvable(l):
+    ass =list(set(l[0]) & set(l[3]))
+    bs = list(set(l[1]) & set(l[3]))
+    cs = list(set(l[2]) & set(l[3]))
+    ds = list(set(l[0]) & set(l[4]))
+    es = list(set(l[1]) & set(l[4]))
+    fs = list(set(l[2]) & set(l[4]))
+    gs = list(set(l[0]) & set(l[5]))
+    hs = list(set(l[1]) & set(l[5]))
+    iss =list(set(l[2]) & set(l[5]))
 
 
+    """
+    print(ass)
+    print(bs)
+    print(cs)
+    print(ds)
+    print(es)
+    print(fs)
+    print(gs)
+    print(hs)
+    print(iss)
+    """
+
+    for a in ass:
+        for b in bs:
+            for c in cs:
+                for d in ds:
+                    for e in es:
+                        for f in fs:
+                            for g in gs:
+                                for h in hs:
+                                    for i in iss:
+                                        
+                                        if allUnique([a,b,c,d,e,f,g,h,i]):
+                                            return True
+    return False
 
 
 connection = sqlite3.connect("stateinfo.db")
@@ -8,13 +49,25 @@ connection = sqlite3.connect("stateinfo.db")
 cursor = connection.cursor()
 
 #this table needs to exist first 
-#possibleCategories = cursor.execute("SELECT id, name, clause FROM categories").fetchall()
+possibleCategories = cursor.execute("SELECT id, name, clause FROM categories WHERE clause is not null").fetchall()
+#possibleCategories.pop()
+print(possibleCategories)
 
 #randomly select possibleCategories
+while True:
+    categories = random.sample(possibleCategories, 6)
+    allAnswers = []
 
-listOfStates = cursor.execute("SELECT name FROM STATES") .fetchall() 
-#needed cuz fetchall is returning a list of tuples
-states = list(map( lambda x: x[0], listOfStates))
+    for category in categories:
+        #print(category[2])
 
-print(states)
+        listOfStates = cursor.execute(str(category[2])) .fetchall()
+        states = list(map( lambda x: x[0], listOfStates))
 
+        #print(states)
+        allAnswers.append(states)
+
+    if CheckIfSolvable(allAnswers):
+        print(categories)
+    else:
+        print("failed")
