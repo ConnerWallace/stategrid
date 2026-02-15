8a059b79d8b9576a6e08dcfce662993b8ea4b88e
connerwallace <theconnerwallace@gmail.com>
2026-02-15 17:54:07 -0500

    rewrites storage

--- Components/DB/StoredData.cs
+++ Components/DB/StoredData.cs
@@ -7,7 +7,7 @@ namespace BlazorApp1.Components.DB
         public string[] answers = new string[9];
         public int guessesLeft  = 10;
         public bool infiniteGuesses = false;
-        public bool UserInfinite = false;
+        public bool UsedInfinite = false;
         public List<string>[] AllGivenAnswers = [.. new List<string>[9].Select(item => new List<string>())];
     }
 }
