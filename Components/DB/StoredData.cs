8f4d832eda13b8857ef6cfd3fe33abae0d6517ec
connerwallace <theconnerwallace@gmail.com>
2026-02-15 15:19:49 -0500

    makes it easier to adjust the displayed order of the list of descriptions

---
+++ Components/DB/StoredData.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Metadata;
+
+namespace BlazorApp1.Components.DB
+{
+    public class StoredData
+    {
+        public string[] answers = new string[9];
+        public int guessesLeft  = 10;
+        public bool infiniteGuesses = false;
+        public bool UserInfinite = false;
+        public List<string>[] AllGivenAnswers = [.. new List<string>[9].Select(item => new List<string>())];
+    }
+}
